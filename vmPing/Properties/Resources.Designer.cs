93d3f0d0185482780851e3d3a2bf4886f0cddda5
Ryan Smith <R-Smith@users.noreply.github.com>
2019-04-14 20:35:38 -0700

    Convert logo to xaml

--- vmPing/Properties/Resources.Designer.cs
+++ vmPing/Properties/Resources.Designer.cs
@@ -229,15 +229,5 @@ namespace vmPing.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap vmPing_96 {
-            get {
-                object obj = ResourceManager.GetObject("vmPing_96", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }

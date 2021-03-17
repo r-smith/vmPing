5fe0e10fb4cfde103728842e594cca385502fc73
Ryan Smith <R-Smith@users.noreply.github.com>
2021-03-16 17:03:10 -0700

    Make GUI elements use OS date/time formats

--- vmPing/App.xaml.cs
+++ vmPing/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
 
 namespace vmPing
 {
@@ -7,6 +9,18 @@ namespace vmPing
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // The following code snippet was taken from Stack Overflow answer here:
+            // https://stackoverflow.com/questions/520115/stringformat-localization-issues-in-wpf/520334#520334
+
+            // Ensure the current culture passed into bindings is the OS culture.
+            // By default, WPF uses en-US as the culture, regardless of the system settings.
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                  typeof(FrameworkElement),
+                  new FrameworkPropertyMetadata(
+                      XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
         // DEBUG: Use this for testing alternate locales.
         //public App()
         //{

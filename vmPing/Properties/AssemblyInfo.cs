4b165a91426cb395459992c9c749a953883962bb
Ryan Smith <R-Smith@users.noreply.github.com>
2022-03-03 10:39:58 -0800

    Version bump

--- vmPing/Properties/AssemblyInfo.cs
+++ vmPing/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("vmPing")]
-[assembly: AssemblyCopyright("Copyright © 2021 Ryan Smith")]
+[assembly: AssemblyCopyright("Copyright © 2022 Ryan Smith")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.18")]
-[assembly: AssemblyFileVersion("1.3.18")]
+[assembly: AssemblyVersion("1.3.19")]
+[assembly: AssemblyFileVersion("1.3.19")]

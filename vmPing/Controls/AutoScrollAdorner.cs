af5d646cc47fda89e18bf7ef30cbc2dcacd8dea2
Ryan Smith <R-Smith@users.noreply.github.com>
2022-03-07 10:19:57 -0800

    Custom ListBox autoscroll behavior + adorner

---
+++ vmPing/Controls/AutoScrollAdorner.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace vmPing.Controls
+{
+    class AutoScrollAdorner : Adorner
+    {
+        public AutoScrollAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            IsHitTestVisible = false;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            const double imageWidth = 13.0;
+            const double imageHeight = 11.0;
+            const double xOffset = 36.0;
+            const double yOffset = 14.0;
+            Rect rect = new Rect(
+                AdornedElement.RenderSize.Width - xOffset,
+                AdornedElement.RenderSize.Height - yOffset,
+                imageWidth,
+                imageHeight);
+            drawingContext.DrawImage((DrawingImage)Application.Current.Resources["icon.down-arrow-scroll-indicator"], rect);
+
+            base.OnRender(drawingContext);
+        }
+    }
+}
--- vmPing/Controls/AutoScrollListBox.cs
+++ vmPing/Controls/AutoScrollListBox.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -9,6 +10,8 @@ namespace vmPing.Controls
     public class AutoScrollListBox : ListBox
     {
         private bool IsAutoScrollEnabled = true;
+        private AdornerLayer _adornerLayer;
+        private AutoScrollAdorner _autoScrollAdorner;
 
         public AutoScrollListBox()
         {
@@ -21,11 +24,49 @@ namespace vmPing.Controls
             // This is to handle dragging/dropping probes.
             if (Items.Count > 1) ScrollIntoView(Items[Items.Count - 1]);
 
-            // Find ScrollViewer child in the ListBox and subscribe to its LostMouseCapture event.
+            // Subscribe to the ScrollViewers LostMouseCapture and ScrollChanged events.
             if (VisualTreeHelper.GetChildrenCount(this) > 0 && VisualTreeHelper.GetChild(this, 0) is Decorator border)
             {
                 ScrollViewer scroll = border.Child as ScrollViewer;
                 scroll.LostMouseCapture += Scroll_LostMouseCapture;
+                scroll.ScrollChanged += Scroll_ScrollChanged;
+            }
+
+            _adornerLayer = AdornerLayer.GetAdornerLayer(this);
+            _autoScrollAdorner = new AutoScrollAdorner(this);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            if (_adornerLayer != null && _autoScrollAdorner != null && VisualTreeHelper.GetChild(this, 0) is Decorator border)
+            {
+                ScrollViewer scroll = border.Child as ScrollViewer;
+                if (scroll.ComputedVerticalScrollBarVisibility != Visibility.Visible)
+                {
+                    IsAutoScrollEnabled = true;
+                    _adornerLayer.Remove(_autoScrollAdorner);
+                }
+
+                if (IsAutoScrollEnabled && !scroll.IsMouseCaptureWithin)
+                {
+                    scroll?.ScrollToEnd();
+                }
+            }
+            base.OnRender(drawingContext);
+        }
+
+        private void Scroll_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            ScrollViewer scrollViewer = (ScrollViewer)sender;
+            if (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
+            {
+                _adornerLayer.Remove(_autoScrollAdorner);
+
+            }
+            else
+            {
+                if (_adornerLayer.GetAdorners(this) == null)
+                    _adornerLayer.Add(_autoScrollAdorner);
             }
         }
 
@@ -35,7 +76,10 @@ namespace vmPing.Controls
             // Check the position of the scrollbar - If it's scrolled to
             // the bottom, then automatically re-enable auto-scrolling.
             ScrollViewer scrollViewer = (ScrollViewer)sender;
-            IsAutoScrollEnabled = scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight;
+            IsAutoScrollEnabled = scrollViewer.VerticalOffset >=
+                (scrollViewer.ScrollableHeight > 100
+                ? scrollViewer.ScrollableHeight - 2
+                : scrollViewer.ScrollableHeight - 1);
         }
 
         protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
@@ -45,7 +89,7 @@ namespace vmPing.Controls
             if (e.Action == NotifyCollectionChangedAction.Add)
             {
                 // An item was added to the ListBox.
-                if (VisualTreeHelper.GetChild(this, 0) is Decorator border)
+                if (IsAutoScrollEnabled && VisualTreeHelper.GetChild(this, 0) is Decorator border)
                 {
                     // Scroll to the bottom of the ListBox. If user is currently clicking the scrollbar, then do nothing.
                     ScrollViewer scroll = border.Child as ScrollViewer;

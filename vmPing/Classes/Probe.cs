69c832881b08412e22d64f69954824fa776af599
Ryan Smith <R-Smith@users.noreply.github.com>
2025-11-04 09:59:00 -0800

    feat: add a "comment" probe type
    Added a "comment" type. Any hostname beginning with the `#` character is ignored.
    
    Implements #64

--- vmPing/Classes/Probe.cs
+++ vmPing/Classes/Probe.cs
@@ -29,7 +29,8 @@ namespace vmPing.Classes
     {
         Dns,
         Ping,
-        Traceroute
+        Traceroute,
+        Comment
     }
 
     public partial class Probe : INotifyPropertyChanged
